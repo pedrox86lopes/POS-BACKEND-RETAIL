aa66f8e888dacdf9b7fc600a9ee8cfa241e9524c
Pedro Lopes <pedro.secinfo@gmail.com>
2025-06-03 10:12:31 +0100

    A Simple JWT Auth

---
+++ JWT/app.py
@@ -0,0 +1,41 @@
+import jwt
+import os
+from datetime import datetime, timedelta
+from flask import Flask, request, jsonify 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+app = Flask(__name__) # Flask Instance required for the application
+
+# LOAD SECRET_KEY from environment variables
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+# JUST TO CHECK IF .ENV exists
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY not defined. Create a .env with SECRET_KEY='STRONG_KEY_'.")
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    if not data:
+        return jsonify(message="NO LOGIN DATA PROVIDED"), 400
+    if "username" not in data or "password" not in data:
+        return jsonify(message="NO USERNAME OR PASSWORD PROVIDED!"), 400
+
+    # Simulating a user authentication
+    if data["username"] == "admin" and data["password"] == "PassW0rd1r@98H8":
+        # Generate JWT token
+        token = jwt.encode(
+            {"user": data["username"], "exp": datetime.now() + timedelta(minutes=30)},
+            SECRET_KEY,
+            algorithm="HS256"
+        )
+        return jsonify(message="WELCOME BABY!", token=token)
+
+    return jsonify(message="wrong!"), 401
+
+# 
+if __name__ == "__main__":
+    app.run(debug=True) # debug=True for development
